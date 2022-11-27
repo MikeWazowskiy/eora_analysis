826b47e153a0d615c7cd5da26fc1c2f8011395a2
MikeWazowskiy <109314704+MikeWazowskiy@users.noreply.github.com>
2022-11-27 18:07:31 +0300

    finished

---
+++ main.py
@@ -0,0 +1,7 @@
+def answer(score):
+    if score >= 86.579714:
+        print('correct')
+    elif score >= 37.592483 and score <= 86.579714:
+        print('reask')
+    else:
+        print('operator')
